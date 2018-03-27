5a73bef79299cc923c57f6c1b869efe9ff07d9de
Leah Miller <leahmiller1969@gmail.com>
2018-03-27 12:26:33 -0700

    added switch statment to finish the loop. click button and count adds one

--- src/store.js
+++ src/store.js
@@ -5,16 +5,25 @@ import {createStore} from 'redux';
 //define state of the application
 const initialState = {
   count: 3   // sets the initial state for app
-}
+};
 
 //need to create reducer (reducer is a function that takes 2 arguments, takes state and action)
-const reducer = (state=initialState, action) =>{   //state=initialState, first time this is run there wont be an initial state. use this so it becomes the initialstate after first run.
-  console.log('reducer', action)
-  return state;
+const reducer = (state = initialState, action) => {   //state=initialState, first time this is run there wont be an initial state. use this so it becomes the initialstate after first run.
+  console.log('reducer', action);
+
+  switch (action.type) { 
+    case 'INCREMENT':
+      return Object.assign({}, state /*initialState*/, { count: state.count + 1 });   // return brand new state object / object.assign / pass an object and then the state. copies values from INCREMENT and puts into new object
+    default: 
+      return state;
+  }
 }
+
+
 
 const store = createStore(reducer);
 
 export default store;
 
 //no way of changing data within store.js
+
